6c3ef55c635ae53178bacb566c2c371a0f39ff9b
Doğuhan Ay <63362475+doguhan2222@users.noreply.github.com>
2020-06-17 23:13:16 +0300

    Add files via upload

---
+++ CaesarEncryptionMethod.java
@@ -0,0 +1,36 @@
+package caesar.encryption.method;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author doguhan
+ */
+public class CaesarEncryptionMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner getData = new Scanner(System.in);
+        
+        System.out.println("Enter Number for Encryption");
+        int number = getData.nextInt();
+     
+        System.out.println("Enter the Word");
+        String word = getData.next();
+        
+        for(int i = 0; i<=word.length()-1; i++){
+            
+           
+            char ch = (char) (word.charAt(i)+number);
+            int ascii = ch;
+            
+            System.out.print((char)ascii);
+            
+        
+        }
+        
+    }
+    
+}
